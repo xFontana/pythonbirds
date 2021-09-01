147f27b367a3d6559e5000ec4f8eedb5952ae6a6
Lucas <lucasfontana2009@hotmail.com>
2021-09-01 19:07:06 -0300

    Criado instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lidiane')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Lucas'
+    print(p.nome)
 
